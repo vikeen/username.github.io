12451a892d5c88c0c6c239d9d02c2f1ae5f9c670
John Rake <john.rake12@gmail.com>
2015-10-17 10:02:14 -0500

    Adding simple-weather documentation

---
+++ simple-weather/api.js
@@ -0,0 +1,22 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "current"
+    ],
+    "modules": [
+        "current",
+        "v2.5"
+    ],
+    "allModules": [
+        {
+            "displayName": "current",
+            "name": "current"
+        },
+        {
+            "displayName": "v2.5",
+            "name": "v2.5"
+        }
+    ],
+    "elements": []
+} };
+});
